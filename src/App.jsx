7b310b26f3d99587b2dd2b5e45fea04460913473
merndevreza <merndevreza@gmail.com>
2024-03-05 17:44:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-center py-4 bg-green-300">
+        React Redux Crash Course
+      </h1>
+    </> 
+  );
+}
+
+export default App;
